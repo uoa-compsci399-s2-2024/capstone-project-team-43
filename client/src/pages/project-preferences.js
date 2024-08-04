e143554991f2bdb4740eb971f521605d1c791333
jli621 <lijade761@gmail.com>
2024-08-05 03:40:43 +1200

    made page for project preference form

--- client/src/pages/project-preferences.js
+++ client/src/pages/project-preferences.js
@@ -4,7 +4,22 @@ import '../App.css';
 const ProjectPreferences = ()=>{
     return(
         <div className="projectPreferences">
-            <div className="preferenceProjects"></div>
+            <h2>Project Preferences</h2>
+            <br></br>
+            <div className="preferenceProjects">
+            </div>
+            <div id="sidebuttons">
+                <button>1</button>
+                <br></br><br></br><br></br>
+                <button>2</button>
+                <br></br><br></br><br></br>
+                <button>3</button>
+                <br></br><br></br><br></br>
+                <button>4</button>
+                <br></br><br></br><br></br>
+                <button>5</button>
+            </div>
+            {/*<button>Submit</button>*/}
         </div>
     );
 };

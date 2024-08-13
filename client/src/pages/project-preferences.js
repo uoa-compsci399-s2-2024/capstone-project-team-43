2c6f67242b40c64bfb91eb1202e0107c0e1d018c
jli621 <lijade761@gmail.com>
2024-08-13 16:01:20 +1200

    made submission page for project preferences

--- client/src/pages/project-preferences.js
+++ client/src/pages/project-preferences.js
@@ -2,25 +2,41 @@ import React from "react";
 import '../App.css';
 
 const ProjectPreferences = ()=>{
+
+    const submit =() =>{
+        document.getElementById('projectPreferenceselements').style.display = "none";
+        document.getElementById('onsubmission').style.display = "block";
+    }
+
     return(
-        <div className="projectPreferences">
-            <h2>Project Preferences</h2>
-            <br></br>
-            <div className="preferenceProjects">
+        <div>
+            <div className="projectPreferences" id = "projectPreferenceselements">
+                <h2>Project Preferences</h2>
+                <br></br>
+                <div className="preferenceProjects">
+                </div>
+                <div id="sidebuttons">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                </div>
+                <div id = "preferencesubmit">
+                <button onClick={submit}>Submit</button>
+                </div>
+                <div id="agreeon">
+                <input type="checkbox" id="agreeupon" name="agreeupon" value="y/n"></input>
+                <label for="agreeupon"> Do all team members agree on the order of the projects provided above?</label>
+                </div>       
             </div>
-            <div id="sidebuttons">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-            </div>
-            <div id = "preferencesubmit">
-            <button>Submit</button>
-            </div>
-            <div id="agreeon">
-            <input type="checkbox" id="agreeupon" name="agreeupon" value="y/n"></input>
-            <label for="agreeupon"> Do all team members agree on the order of the projects provided above?</label>
+            <div id = "onsubmission">
+                <h2>Your project preferences have been submitted!</h2>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <div id = "return">
+                <button>Return to Projects</button>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                </div>
             </div>
         </div>
     );

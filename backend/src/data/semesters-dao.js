93fd14479cbe8da75310e37de1cd0587ec745ac4
jli621 <lijade761@gmail.com>
2024-09-10 09:54:05 +1200

    Merge branch 'main' into Frontend_jade

--- backend/src/data/semesters-dao.js
+++ backend/src/data/semesters-dao.js
@@ -8,17 +8,42 @@ const DB_NAME = process.env.DB_NAME;
 
 /**
  * @typedef {object} Semester //defines the semester object
+ * @property {number} id
  * @param {Date} start_date
  * @param {Date} end_date
- * @param {string} semester_name
+ * @param {boolean} is_semester_one
  */
+/**
+ * Gets all semesters
+ *
+ * @returns {Promise<Semester[]>}
+ */
+export async function getSemesters() {
+  let connection;
+  try {
+    // Get connection from pool
+    connection = await pool.getConnection();
 
+    await connection.query(`USE ${DB_NAME};`);
+
+    /** @type {Semester[]} */
+    const semester = await connection.query('SELECT * FROM SEMESTER');
+
+    // If there is a connection, release it
+    if (connection) connection.release();
+
+    return semester;
+
+  } catch (err) {
+    console.error('Error executing query/s:', err.message);
+  }
+}
 /**
  * Gets a semester given an id
  *
  * @returns {Promise<Semester>}
  */
-export async function retrieveSemester(id) {
+export async function getSemester(id) {
   let connection;
   try {
     // Get connection from pool
@@ -27,7 +52,7 @@ export async function retrieveSemester(id) {
     await connection.query(`USE ${DB_NAME};`);
 
     /** @type {Semester} */
-    const semester = await connection.query('SELECT * FROM SEMESTER_DATES WHERE id = ?', [id]);
+    const semester = await connection.query('SELECT * FROM SEMESTER WHERE id = ?', [id]);
 
     console.log('Semester:', semester);
 
@@ -42,14 +67,39 @@ export async function retrieveSemester(id) {
 }
 
 /**
+ * Gets all semesters
+ *
+ * @returns {Promise<Semester[]>}
+ */
+export async function retrieveSemesters() {
+  let connection;
+  try {
+    // Get connection from pool
+    connection = await pool.getConnection();
+
+    await connection.query(`USE ${DB_NAME};`);
+    const [rows] = await connection.query('SELECT * FROM SEMESTER_DATES');
+    console.log('Rows:', rows);
+
+    // If there is a connection, release it
+    if (connection) connection.release();
+
+    return rows;
+
+  } catch (err) {
+    console.error('Error executing query/s:', err.message);
+  }
+}
+
+/**
  * Creates a new semester 
  * @param {Date} start_date
  * @param {Date} end_date
- * @param {boolean} semester_name //String visualization of semester, ie. "2024 Sem 2"
+ * @param {boolean} is_semester_one //String visualization of semester, ie. "2024 Sem 2"
  *
  * @return the newly created project
  */
-export async function createSemester(start_date, end_date, semester_one) {
+export async function createSemester(start_date, end_date, is_semester_one) {
   let connection;
   try {
 
@@ -60,10 +110,10 @@ export async function createSemester(start_date, end_date, semester_one) {
 
     // Insert semester into db
     const response = await connection.query(
-      "INSERT INTO SEMESTER_DATES (start_date, end_date, semester_one) VALUES (?, ?, ?)", [start_date, end_date, semester_one]);
+      "INSERT INTO SEMESTER (start_date, end_date, is_semester_one) VALUES (?, ?, ?)", [start_date, end_date, is_semester_one]);
 
     /** @type {Semester} */
-    const semester = await connection.query("SELECT * FROM SEMESTER_DATES WHERE id = ?", [response.insertId]); // insertId is the auto-generated Primary Key value
+    const semester = await connection.query("SELECT * FROM SEMESTER WHERE id = ?", [response.insertId]); // insertId is the auto-generated Primary Key value
 
     // If there is a connection, release it
     if (connection) connection.release();
@@ -81,9 +131,9 @@ export async function createSemester(start_date, end_date, semester_one) {
  * @param {number} id // The id of the semester to update
  * @param {Date} start_date // The new start_date of semester
  * @param {Date} end_date // The new end_date of semester
- * @param {boolean} semester_one // Boolean if its semester one or not
+ * @param {boolean} is_semester_one // Boolean if its semester one or not
  */
-export async function updateSemester(id, start_date, end_date, semester_one) {
+export async function updateSemester(id, start_date, end_date, is_semester_one) {
   let connection;
   try {
 
@@ -92,7 +142,7 @@ export async function updateSemester(id, start_date, end_date, semester_one) {
 
     await connection.query(`USE ${DB_NAME};`);
 
-    await connection.query("UPDATE SEMESTER_DATES SET start_date = ?, end_date = ?, semester_one = ? WHERE id = ?", [start_date, end_date, semester_one, id]);
+    await connection.query("UPDATE SEMESTER SET start_date = ?, end_date = ?, is_semester_one = ? WHERE id = ?", [start_date, end_date, is_semester_one, id]);
 
     // If there is a connection, release it
     if (connection) connection.release();
@@ -116,7 +166,7 @@ export async function deleteSemester(id) {
   await connection.query(`USE ${DB_NAME};`);
 
   try {
-    await connection.query("DELETE FROM SEMESTER_DATES WHERE id = ?", id);
+    await connection.query("DELETE FROM SEMESTER WHERE id = ?", id);
   } catch (err) {
     console.error('Error executing query:', err);
   }

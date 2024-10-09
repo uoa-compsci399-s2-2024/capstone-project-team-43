93756274e25f0480ca5bf00e3040192ee042129b
Aleks Cheifetz <aleksandr.cheifetz@gmail.com>
2024-10-09 15:40:48 +1300

    Use relative URL path in api/auth

--- src/routes/api/api-authentication.js
+++ src/routes/api/api-authentication.js
@@ -231,7 +231,7 @@ router.get('/google/role/:role', async (req, res) => {
         res.redirect(authorizeUrl);
     } catch (error) {
         console.log("Error authenticating: " + error);
-        res.status(500).redirect('http://localhost:3001/');
+        res.status(500).redirect('/');
     }
 });
 
@@ -242,7 +242,7 @@ router.get('/google/callback', async (req, res) => {
         const { state: role } = req.query;
 
         if (!Authcode) {
-            return res.status(400).redirect('http://localhost:3001/');
+            return res.status(400).redirect('/');
         }
 
         // Exchange Google's authorization code to get tokens
@@ -280,7 +280,7 @@ router.get('/google/callback', async (req, res) => {
                 path: '/'
             }));
 
-            return res.status(200).redirect('http://localhost:3001/');
+            return res.status(200).redirect('/');
 
 
         } else if (user_role != null && role == user_role) {
@@ -296,7 +296,7 @@ router.get('/google/callback', async (req, res) => {
                 path: '/'
             }));
 
-            return res.status(200).redirect('http://localhost:3001/');
+            return res.status(200).redirect('/');
 
         } else {
 
@@ -310,7 +310,7 @@ router.get('/google/callback', async (req, res) => {
                 path: '/'
             }));
 
-            return res.status(401).redirect('http://localhost:3001/');
+            return res.status(401).redirect('/');
         }
 
     } catch (error) {

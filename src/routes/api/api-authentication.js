558f17c04d339d9b85108b0e29eaf6f0bc37cf47
eyal132 <38598729+eyal132@users.noreply.github.com>
2024-10-16 20:09:00 +1300

    Reverted DEV auth changes.

--- src/routes/api/api-authentication.js
+++ src/routes/api/api-authentication.js
@@ -14,6 +14,8 @@ dotenv.config();
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
+const DEV_EMAIL = process.env.DEV_EMAIL;
+const DEV_USER_ROLE = process.env.DEV_USER_ROLE;
 
 const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
@@ -227,6 +229,12 @@ router.get('/google/callback', async (req, res) => {
         // Checks if the user exists in the database, if so it returns the role, if not returns null
         let user_role = await findUser(email);
 
+        let DEVAuthorizedUsers = [DEV_EMAIL]; // **MUST REMOVE BEFORE DEPLOYMENT ***
+        if (email.includes(DEVAuthorizedUsers)) {
+            user_role = role;
+            console.log("DEV logging in, authorizing access, role: ", user_role);
+        }
+
         let token = null
 
         if (user_role == null && role == "client") {
@@ -313,6 +321,12 @@ router.get("/role", async (req, res) => {
         };
 
         const db_role = await findUser(user.email);
+
+        let DEVAuthorizedUsers = [DEV_EMAIL]; // **MUST REMOVE BEFORE DEPLOYMENT ***
+        if (user.email.includes(DEVAuthorizedUsers)) {
+            console.log("DEV checking role, authorizing access with role: ", user.role);
+            return res.status(200).json({ role: user.role });
+        }
 
         if (db_role == user.role) {
 
